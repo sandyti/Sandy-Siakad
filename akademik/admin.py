7bdf785e98b506133096101bf7a2c1e5e036c8a7
sandyti <sandytriantono816@gmail.com>
2024-12-11 10:03:27 +0700

    first commit

--- akademik/admin.py
+++ akademik/admin.py
@@ -3,20 +3,20 @@ from unfold.admin import ModelAdmin
 from .models import TahunAkademik, MataKuliah, Jadwal, KRS
 # Register your models here.
 @admin.register(TahunAkademik)
-class TahunAkademikAdmin(admin.ModelAdmin):
+class TahunAkademikAdmin(ModelAdmin):
     list_display = ('tahun', 'semester')
     search_fields = ('tahun', 'semester')
 
 
 @admin.register(MataKuliah)
-class MataKuliahAdmin(admin.ModelAdmin):
+class MataKuliahAdmin(ModelAdmin):
     list_display = ('kode', 'nama', 'prodi', 'sks')
     search_fields = ('kode', 'nama', 'prodi')
     list_filter = ('prodi',)
 
 
 @admin.register(Jadwal)
-class JadwalAdmin(admin.ModelAdmin):
+class JadwalAdmin(ModelAdmin):
     list_display = ('mata_kuliah', 'dosen', 'hari_pembelajaran', 'jam_mulai', 'jam_selesai', 'tahun_akademik', 'ruangan', 'kuota')
     search_fields = ('dosen', 'mata_kuliah__nama', 'ruangan')
     list_filter = ('hari_pembelajaran', 'tahun_akademik')
@@ -24,7 +24,7 @@ class JadwalAdmin(admin.ModelAdmin):
 
 
 @admin.register(KRS)
-class KRSAdmin(admin.ModelAdmin):
+class KRSAdmin(ModelAdmin):
     list_display = ('mahasiswa', 'tahun_akademik')
     search_fields = ('mahasiswa',)
     list_filter = ('tahun_akademik',)
